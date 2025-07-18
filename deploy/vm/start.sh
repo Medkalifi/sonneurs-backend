0e0d266f73db11985c20696c43870eeffb880cca
Mohammed EL KHALIFI <mohammed.elkhalifi@ext.adeo.com>
2025-07-18 11:25:38 +0200

    Supprime start.sh (généré dynamiquement dans deploy-vm-full.sh)

--- deploy/vm/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APP_JAR="app.jar"
-LOG_FILE="app.log"
-DB_PASSWORD="$1"
-
-echo ">>> Lancement de l’application Java..."
-exec java -jar "$APP_JAR" --spring.datasource.password="$DB_PASSWORD"  > "$LOG_FILE" 2>&1
